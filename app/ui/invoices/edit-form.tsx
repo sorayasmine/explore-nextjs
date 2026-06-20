5fa3e8a10dbd79d3f191718be142594ab18497c6
sorayasmine <niceefforts42@gmail.com>
2026-06-20 20:54:06 +0700

    feat(invoices): implement crud

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { updateInvoice } from '@/app/lib/actions';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -14,11 +14,13 @@ export default function EditInvoiceForm({
   invoice,
   customers,
 }: {
-  invoice: InvoiceForm;
-  customers: CustomerField[];
+  readonly invoice: InvoiceForm;
+  readonly customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -7,9 +7,8 @@ import { fetchFilteredInvoices } from '@/app/lib/data';
 export default async function InvoicesTable({
   query,
   currentPage,
-}: {
-  query: string;
-  currentPage: number;
+}: { readonly query: string;
+  readonly currentPage: number;
 }) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
