2037b267c27309848c7a7347bf3f60681bbf8b00
sorayasmine <niceefforts42@gmail.com>
2026-06-24 11:09:00 +0700

    feat(customer): integrate and slicing customer page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -167,24 +167,25 @@ export async function fetchInvoiceById(id: string) {
   }
 }
 
-export async function fetchCustomers() {
+export async function fetchCustomers(query: string) {
   try {
-    const customers = await sql<CustomerField[]>`
-      SELECT
-        id,
-        name
+    const customers = await sql`SELECT COUNT(*)
       FROM customers
-      ORDER BY name ASC
+      WHERE customers.name ILIKE ${`%${query}%`} OR
+      customers.email ILIKE ${`%${query}%`}
     `;
 
-    return customers;
+    const totalPages = Math.ceil(Number(customers[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch all customers.');
   }
 }
 
-export async function fetchFilteredCustomers(query: string) {
+export async function fetchFilteredCustomers(query: string, currentPage: number,) {
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
   try {
     const data = await sql<CustomersTableType[]>`
 		SELECT
@@ -202,6 +203,7 @@ export async function fetchFilteredCustomers(query: string) {
         customers.email ILIKE ${`%${query}%`}
 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
 		ORDER BY customers.name ASC
+    LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
 	  `;
 
     const customers = data.map((customer) => ({
